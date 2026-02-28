cf3b4876168cc48e4f794a180d24d2fb75fe30f9
ATOMMAX-2001 <atomcoder2001@gmail.com>
2026-03-01 02:01:08 +0530

    + params & query & serve static files

--- slimeweb/test/test.py
+++ slimeweb/test/test.py
@@ -3,11 +3,17 @@ from lib import slime
 app = slime.Slime(__file__)
 
 
-@app.route(path="/", method="GET")
+@app.route(path="/", method="POST")
 def hello(req, resp):
-    html = req.render("hello.html", **{"name": "abilash", "age": 24})
+    print(req.query)
+    print(req.params)
+    print(req.body)
+    # for i in ["abilash", "abi", "atom"]:
+    html = req.render("hello.html", **{"name": "abi", "age": 24})
     return resp.html(html)
-    # return resp.json({"name": "abilash", "age": 24})
+
+
+# return resp.json({"name": "abilash", "age": 24})
 
 
 if __name__ == "__main__":
